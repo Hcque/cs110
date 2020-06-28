a2f0aa606f210e305c7e15739df6615c74a87d6c
Hcque <hc995639@gmail.com>
2020-06-28 10:02:12 +0800

    p1

--- assign3/pipeline.c
+++ assign3/pipeline.c
@@ -6,57 +6,31 @@
 
 #include "pipeline.h"
 #include <stdio.h>
+#include <unistd.h>
 #include <stdbool.h>
 #include <errno.h>
 
 void pipeline(char *argv1[], char *argv2[], pid_t pids[]) {
-  // fds[0] - read, fds[1] - write
-  int fds[2];
-  pipe(fds);
-
-  pid_t reader = fork();
-  if (reader == -1) {
-    fprintf(stderr, "Failed to create reader process.\n");
-    return;
-  }
-
-  if (!reader) {
-    // In child reader process
-    // close write end of pipe
-    close(fds[1]);
-
-    // exec argv2 using fds[0] as stdin
-    dup2(fds[0], STDIN_FILENO);
-    close(fds[0]);
-    execvp(argv2[0], argv2);
-  }
-
-  // In parent pipeline process
-  pids[1] = reader;
-
-  // close fds[0] since child reader already has it
-  close(fds[0]);
-
-  pid_t writer = fork();
-  if (writer == -1) {
-    fprintf(stderr, "Failed to create writer process.\n");
-    return;
-  }
-
-  if (!writer) {
-    // In child writer process
-    // close read end of pipe
-    close(fds[0]);
-
-    // exec argv1 using fds[1] as stdout
-    dup2(fds[1], STDOUT_FILENO);
-    close(fds[1]);
-    execvp(argv1[0], argv1);
-  }
-
-  // close fds[1] since child writer already has it
-  close(fds[1]);
-
-  // In parent pipeline process again
-  pids[0] = writer;
+    int fds[2];
+    pipe(fds);
+    pid_t pid1 = fork();
+    if (pid1 == 0) {
+        printf("child1\n");
+        close(fds[0]);
+        dup2(fds[1], STDOUT_FILENO);
+        close(fds[1]);
+        execvp(argv1[0], argv1);
+        printf("exit abnormlly.\n");
+    }
+    sleep(2);
+    pid_t pid2 = fork();
+    if (pid2 == 0) {
+        printf("child2\n");
+        close(fds[1]);
+        dup2(fds[0], STDIN_FILENO);
+        close(fds[0]);
+        execvp(argv2[0], argv2);
+    }
+    pids[0] = pid1;
+    pids[1] = pid2;
 }
